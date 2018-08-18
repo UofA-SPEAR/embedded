c6fc98784d39ec5997a0fcc55347f1c68b8b1432
David Lenfesty <lenfesty@ualberta.ca>
2018-08-17 23:12:10 -0600

    Took out RTOS stuff.
    I think I'm closer to getting it to compile.

--- freertos_libuavcan_example/src/main.cpp
+++ freertos_libuavcan_example/src/main.cpp
@@ -1,13 +1,12 @@
 // Stuff we need to define
-#ifndef UAVCAN_STM32_FREERTOS
-#define UAVCAN_STM32_FREERTOS 1
+#ifndef UAVCAN_STM32_BAREMETAL
+#define UAVCAN_STM32_BAREMETAL 1
 #endif
 
 #ifndef UAVCAN_STM32_NUM_IFACES
 #define UAVCAN_STM32_NUM_IFACES 1
 #endif
 
-#include "FreeRTOS.h"
 #include <uavcan_stm32/uavcan_stm32.hpp>
 
 
