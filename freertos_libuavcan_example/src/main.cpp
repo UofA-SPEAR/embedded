da480cf2218485cff97e05c745d9228f007fe602
David Lenfesty <lenfesty@ualberta.ca>
2018-08-17 22:58:44 -0600

    Compiled DSDL files

--- freertos_libuavcan_example/src/main.cpp
+++ freertos_libuavcan_example/src/main.cpp
@@ -1,4 +1,15 @@
-#include <uavcan_stm32.hpp>
+// Stuff we need to define
+#ifndef UAVCAN_STM32_FREERTOS
+#define UAVCAN_STM32_FREERTOS 1
+#endif
+
+#ifndef UAVCAN_STM32_NUM_IFACES
+#define UAVCAN_STM32_NUM_IFACES 1
+#endif
+
+#include "FreeRTOS.h"
+#include <uavcan_stm32/uavcan_stm32.hpp>
+
 
 int main() {
   // nothing for now
