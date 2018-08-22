709f8c36b247b75dd7f1ae5554f99b9645895fd2
David Lenfesty <lenfesty@ualberta.ca>
2018-08-21 22:33:20 -0600

    Finished skeleton for UAVCAN subscribers.

--- libuavcan_example/Inc/app/subscribers.hpp
+++ libuavcan_example/Inc/app/subscribers.hpp
@@ -1,6 +1,20 @@
 #ifndef APP_SUBSCRIBERS_HPP_
 #define APP_SUBSCRIBERS_HPP_
 
+#include <app/uavcan.hpp>
+
+// Message type includes
+
+// Subscriber objects - keep static, only access through these functions
+
+/**@brief Function to initialize UAVCAN subscribers.
+ *
+ */
+uint8_t subscribers_init() {
+	uint8_t rc = 0;
+
+	return rc;
+}
 
 
 #endif
