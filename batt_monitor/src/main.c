3f577d8c347104edd47e6dc018bc044e3884cc46
David Lenfesty <lenfesty@ualberta.ca>
2019-05-15 17:20:05 -0600

    Added GPIO stuff

--- batt_monitor/src/main.c
+++ batt_monitor/src/main.c
@@ -2,29 +2,55 @@
 #include "stm32f3xx_hal.h"
 
 
+/*** @brief General GPIO Initialization
+ * 
+ */
+void gpio_init() {
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	/* ------ Inputs ------ */
+	// Node select
+	GPIO_InitTypeDef gpio;
+	gpio.Pin = GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9;
+	gpio.Mode = GPIO_MODE_INPUT;
+	gpio.Pull = GPIO_PULLDOWN;
+	HAL_GPIO_Init(GPIOB, &gpio);
+
+	// User Button
+	gpio.Pin = GPIO_PIN_1;
+	gpio.Pull = GPIO_PULLUP;
+	HAL_GPIO_Init(GPIOA, &gpio);
+
+	// Analog Inputs
+
+	gpio.Pin = GPIO_PIN_0 | GPIO_PIN_2;
+	gpio.Pull = GPIO_NOPULL;
+	gpio.Mode = GPIO_MODE_ANALOG;
+	HAL_GPIO_Init(GPIOA, &gpio);
+
+	/* ------- Outputs ------ */
+	// Out Enable
+	gpio.Pin = GPIO_PIN_15;
+	gpio.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio.Pull = GPIO_NOPULL;
+	gpio.Speed = GPIO_SPEED_FREQ_HIGH;
+	HAL_GPIO_Init(GPIOA, &gpio);
+
+	// CAN Bus
+	gpio.Pin = GPIO_PIN_11 | GPIO_PIN_12;
+	gpio.Mode = GPIO_MODE_AF_PP;
+	gpio.Speed = GPIO_SPEED_FREQ_HIGH;
+	gpio.Alternate = GPIO_AF7_CAN; // TODO verify this
+	HAL_GPIO_Init(GPIOA, &gpio);
+}
+
 /*
  * Theoretically a working blinky example.
  * Untested though.
  */
 int main(void) {
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	GPIO_InitTypeDef gpio;
-	gpio.Pin = GPIO_PIN_5;
-	gpio.Mode = GPIO_MODE_OUTPUT_PP;
-	gpio.Pull = GPIO_NOPULL;
-	gpio.Speed = GPIO_SPEED_FREQ_LOW;
-
-	HAL_GPIO_Init(GPIOA, &gpio);
-
-	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-
-	HAL_Init();
-    
 	for (;;) {
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);
-		HAL_Delay(500);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
-		HAL_Delay(500);
 	}
 }
 
