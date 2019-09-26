89a8a64eda64d30fa119ea408f773b9bb10a57c3
David Lenfesty <lenfesty@ualberta.ca>
2019-09-25 21:21:27 -0600

    Started work on ChibiOS example.
    Doesn't compile

---
+++ chibios_example/.dep/vectors.o.d
@@ -0,0 +1,8 @@
+build/obj/vectors.o: \
+ ../ChibiOS/os/common/startup/ARMCMx/compilers/GCC/vectors.S \
+ ../ChibiOS/os/common/startup/ARMCMx/devices/STM32F3xx/cmparams.h \
+ ../ChibiOS/os/hal/boards/ST_NUCLEO64_F303RE/board.h
+
+../ChibiOS/os/common/startup/ARMCMx/devices/STM32F3xx/cmparams.h:
+
+../ChibiOS/os/hal/boards/ST_NUCLEO64_F303RE/board.h:
