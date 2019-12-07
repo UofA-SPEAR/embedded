aed64538e0c9f9fdbae51bad1261d219b657c8b6
Jarrett <jarrett21@gmail.com>
2019-12-07 16:41:12 -0700

    Added config for L432 version

--- common/spear_speedy_board/board.mk
+++ common/spear_speedy_board/board.mk
@@ -1,5 +1,5 @@
 # List of all the board related files.
-BOARDSRC = $(COMMON)//spear_speedy_board/board.c
+BOARDSRC = $(COMMON)/spear_speedy_board/board.c
 
 # Required include directories
 BOARDINC = $(COMMON)/spear_speedy_board
---
+++ common/spear_speedy_board_L432/board.mk
@@ -0,0 +1,9 @@
+# List of all the board related files.
+BOARDSRC = $(COMMON)/spear_speedy_board_L432/board.c
+
+# Required include directories
+BOARDINC = $(COMMON)/spear_speedy_board_L432
+
+# Shared variables
+ALLCSRC += $(BOARDSRC)
+ALLINC  += $(BOARDINC)
