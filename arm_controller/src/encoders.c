6af0d795a5aa8c9b5aebadb90f19180daf8a5901
ogodev <evanogo.comm94@gmail.com>
2020-01-22 20:14:39 -0700

    adc implementation; fixed merge error

--- arm_controller/src/encoders.c
+++ arm_controller/src/encoders.c
@@ -3,91 +3,99 @@
 
 #include "encoders.h"
 
-ADC_HandleTypeDef hadc3;
+ADC_HandleTypeDef hadc2;
 // I hope TIM3 isn't enabled anywhere else
 TIM_HandleTypeDef tim3;
 
-void potA_init(void) {
-	ADC_ChannelConfTypeDef sConfig;
-	hadc3.Instance = ADC3;
-	hadc3.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
-	hadc3.Init.Resolution = ADC_RESOLUTION_12B;
-	hadc3.Init.ScanConvMode = ADC_SCAN_DISABLE;
-	hadc3.Init.ContinuousConvMode = DISABLE;
-	hadc3.Init.DiscontinuousConvMode = DISABLE;
-	hadc3.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-	hadc3.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-	hadc3.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-	hadc3.Init.NbrOfConversion = 1;
-	hadc3.Init.DMAContinuousRequests = DISABLE;
-	hadc3.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-	hadc3.Init.LowPowerAutoWait = DISABLE;
-	hadc3.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
+void pot_init(uint8_t motor) {
+	hadc2.Instance = ADC2;
+	hadc2.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+	hadc2.Init.Resolution = ADC_RESOLUTION_12B;
+	hadc2.Init.ScanConvMode = ADC_SCAN_DISABLE;
+	hadc2.Init.ContinuousConvMode = DISABLE;
+	hadc2.Init.DiscontinuousConvMode = DISABLE;
+	hadc2.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+	hadc2.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+	hadc2.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	hadc2.Init.NbrOfConversion = 1;
+	hadc2.Init.DMAContinuousRequests = DISABLE;
+	hadc2.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+	hadc2.Init.LowPowerAutoWait = DISABLE;
+	hadc2.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
 
-	if (HAL_ADC_Init(&hadc3) != HAL_OK) {
+
+	if (HAL_ADC_Init(&hadc2) != HAL_OK) {
 		while(1);
 	}
 
 	// Configure ADC multi-mode
 	ADC_MultiModeTypeDef multimode;
 	multimode.Mode = ADC_MODE_INDEPENDENT;
-	if (HAL_ADCEx_MultiModeConfigChannel(&hadc3, &multimode) != HAL_OK) {
+	if (HAL_ADCEx_MultiModeConfigChannel(&hadc2, &multimode) != HAL_OK) {
 		while(1);
 	}
 
-
-	sConfig.Channel = ADC_CHANNEL_12;
-	sConfig.Rank = ADC_REGULAR_RANK_1;
-	sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
-	sConfig.SingleDiff = ADC_SINGLE_ENDED;
-	sConfig.OffsetNumber = ADC_OFFSET_NONE;
-	sConfig.Offset = 0;
-
-	if (HAL_ADC_ConfigChannel(&hadc3, &sConfig) != HAL_OK) {
-		while(1);
+	/* Motor 0 is PA6, motor 1 is PA7
+	 * channels 3 and 4, respectively
+	 */
+	if (motor == 0) {
+		// Configure channel sample time to be 1.5 cycles
+		ADC2->SMPR1 |= (0b000) << ADC_SMPR1_SMP3_Pos;
+		// Channel is single ended by default
+	} else if (motor == 1) {
+		// Configure channel sample time to be 1.5 cycles
+		ADC2->SMPR1 |= (0b000) << ADC_SMPR1_SMP4_Pos;
+		// Channel is single ended by default
 	}
+
+	// Configure GPIO
+	GPIO_InitTypeDef gpio;
+	gpio.Pin = GPIO_PIN_6 << motor;
+	gpio.Mode = GPIO_MODE_ANALOG;
+	HAL_GPIO_Init(GPIOA, &gpio);
 }
 
 /**
  * returns a number between [0, x) representing the turn
  * returns -1 in an error
  */
-uint32_t potA_read(){
+uint32_t pot_read(uint8_t motor) {
 	static int val;
 	val = -1;
 
-	HAL_ADC_Start(&hadc3);
-	if (HAL_ADC_PollForConversion(&hadc3, 1000) == HAL_OK){
-		val = HAL_ADC_GetValue(&hadc3);
+	// Configure channel sequence for motor
+	ADC2->SQR1 = (3 + motor) << ADC_SQR1_SQ1_Pos;
+
+	HAL_ADC_Start(&hadc2);
+	if (HAL_ADC_PollForConversion(&hadc2, 1000) == HAL_OK){
+		val = HAL_ADC_GetValue(&hadc2);
 	}
-    HAL_ADC_Stop(&hadc3);
+    HAL_ADC_Stop(&hadc2);
 
 	return val;
 }
 
-static void encoderA_gpio_init() {
-	// Not sure if I have to do anything here
-	// I think if I set the pins as input this is handled,
-	// and input is the default state
+static void encoder_gpio_init() {
 	__HAL_RCC_GPIOA_CLK_ENABLE();
-	__HAL_RCC_TIM3_CLK_ENABLE();
+	__HAL_RCC_TIM8_CLK_ENABLE();
 	GPIO_InitTypeDef gpio;
-	gpio.Pin = GPIO_PIN_6 | GPIO_PIN_7;
+	gpio.Pin = GPIO_PIN_6 | GPIO_PIN_8;
 	gpio.Mode = GPIO_MODE_AF_PP;
 	gpio.Speed = GPIO_SPEED_FREQ_HIGH;
 	gpio.Pull = GPIO_PULLUP;
-	gpio.Alternate = GPIO_AF2_TIM3;
+	gpio.Alternate = GPIO_AF10_TIM8;
 
-	HAL_GPIO_Init(GPIOA, &gpio);
+	HAL_GPIO_Init(GPIOB, &gpio);
 }
 
 /** @brief Set up TIM3 in encoder mode.
+ * 
+ * Note here that the pins are not correct for encoder A to work
  */
-void encoderA_init() {
-	encoderA_gpio_init();
+void encoder_init() {
+	encoder_gpio_init();
 
-	// UNTESTED
-	tim3.Instance = TIM3;
+	tim3.Instance = TIM8;
 	tim3.Init.Prescaler = 0;
 	tim3.Init.Period = UINT16_MAX;
 	tim3.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -111,7 +119,7 @@ void encoderA_init() {
 
 	HAL_TIM_Encoder_Init(&tim3, &encoder);
 	HAL_TIM_Encoder_Start(&tim3, TIM_CHANNEL_ALL);
-	TIM3->CNT = ENCODER_START_VAL;
+	TIM8->CNT = ENCODER_START_VAL;
 
 	TIM_MasterConfigTypeDef sMasterConfig;
 
@@ -121,16 +129,76 @@ void encoderA_init() {
 	HAL_TIMEx_MasterConfigSynchronization(&tim3, &sMasterConfig);
 }
 
-// more setup code, this time for the pin
-void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc){
-	if (hadc->Instance == ADC3) {
-		__HAL_RCC_ADC34_CLK_ENABLE();
-		__HAL_RCC_GPIOB_CLK_ENABLE();
+/*
+ * Porting ADC Capabilities to ChibiOS
+ */
 
-		GPIO_InitTypeDef GPIO_InitStruct;
-		GPIO_InitStruct.Pin = POTA_PIN;
-		GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-		GPIO_InitStruct.Pull = GPIO_NOPULL;
-		HAL_GPIO_Init(POTA_PORT, &GPIO_InitStruct);
-	}
+#include "ch.h"
+#include "hal.h"
+
+adcInit();
+adcStart();
+
+static const ADCConversionGroup adcgrpcfg1 = {
+  FALSE,
+  ADC_GRP1_NUM_CHANNELS,
+  NULL,
+  adcerrorcallback,
+  ADC_CFGR_CONT,            /* CFGR    */
+  ADC_TR(0, 4095),          /* TR1     */
+  ADC_CR2_SWSTART,          /* Cr2: SOFTWARE TRIGGER*/
+  {                         /* SMPR[2] */
+    ADC_SMPR1_SMP_AN1(ADC_SMPR_SMP_61P5),
+    0
+  },
+  {                         /* SQR[4]  */
+    ADC_SQR1_SQ1_N(ADC_CHANNEL_IN2),
+    0,
+    0,
+    0
+  }
+};
+/**
+ * @brief   Performs an ADC conversion.
+ * @details Performs a synchronous conversion operation.
+ * @note    The buffer is organized as a matrix of M*N elements where M is the
+ *          channels number configured into the conversion group and N is the
+ *          buffer depth. The samples are sequentially written into the buffer
+ *          with no gaps.
+ *
+ * @param[in] adcp      pointer to the @p ADCDriver object
+ * @param[in] grpp      pointer to a @p ADCConversionGroup object
+ * @param[out] samples  pointer to the samples buffer
+ * @param[in] depth     buffer depth (matrix rows number). The buffer depth
+ *                      must be one or an even number.
+ * @return              The operation result.
+ * @retval MSG_OK       Conversion finished.
+ * @retval MSG_RESET    The conversion has been stopped using
+ *                      @p acdStopConversion() or @p acdStopConversionI(),
+ *                      the result buffer may contain incorrect data.
+ * @retval MSG_TIMEOUT  The conversion has been stopped because an hardware
+ *                      error.
+ *
+ * @api
+ */
+msg_t adcConvert(ADCDriver *adcp,
+                 const ADCConversionGroup *grpp,
+                 adcsample_t *samples,
+                 size_t depth) {
+  ...
 }
+
+/**
+ * @brief   Stops an ongoing conversion.
+ * @details This function stops the currently ongoing conversion and returns
+ *          the driver in the @p ADC_READY state. If there was no conversion
+ *          being processed then the function does nothing.
+ *
+ * @param[in] adcp      pointer to the @p ADCDriver object
+ *
+ * @api
+ */
+void adcStopConversion(ADCDriver *adcp) {
+  ...
+}
+
