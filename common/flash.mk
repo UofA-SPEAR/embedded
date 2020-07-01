e22329dcefb58eb792aa1c025c2496bbfcf3a744
David Lenfesty <lenfesty@ualberta.ca>
2020-06-30 20:48:33 -0600

    common/flash.mk: added option to add OpenOCD search dir

--- common/flash.mk
+++ common/flash.mk
@@ -1,7 +1,11 @@
 # Adds a `flash` target, for the F3
 
+ifdef OPENOCD_SEARCH_DIR
+	OPENOCD_ARGS := -s ${OPENOCD_SEARCH_DIR}
+endif
+
 flash: ./build/ch.elf
-	openocd -f ../common/f3_openocd.cfg -c "program ./build/ch.elf verify reset exit"
+	openocd -f ../common/f3_openocd.cfg -c "program ./build/ch.elf verify reset exit" ${OPENOCD_ARGS}
 
 # Hack so we don't use this as default
 
