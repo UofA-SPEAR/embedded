aa68125cf5a4e7342c8e4f8ce500e8d8ce62e10e
Le Minh Dang <minhdangle7a6@gmail.com>
2021-10-31 09:17:00 -0600

    cleaning up the repo: (#64)
    delete bin/ folder as we have Vagrant as the tool.
    delete L431 variants as we are only using F303s for MCU
    delete CMSIS/device as ChibiOS has supplied them folder
    delete redundant common/Vagrantfile
    delete common/vscode for staging the CMake

--- common/spear_speedy_board/board.mk
+++
@@ -1,9 +0,0 @@
-# List of all the board related files.
-BOARDSRC = $(COMMON)/spear_speedy_board/board.c
-
-# Required include directories
-BOARDINC = $(COMMON)/spear_speedy_board
-
-# Shared variables
-ALLCSRC += $(BOARDSRC)
-ALLINC  += $(BOARDINC)
--- common/spear_speedy_board_L431/board.mk
+++
@@ -1,9 +0,0 @@
-# List of all the board related files.
-BOARDSRC = $(COMMON)/spear_speedy_board_L431/board.c
-
-# Required include directories
-BOARDINC = $(COMMON)/spear_speedy_board_L431
-
-# Shared variables
-ALLCSRC += $(BOARDSRC)
-ALLINC  += $(BOARDINC)
